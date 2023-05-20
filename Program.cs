0b27dc825646f4f8dce5a6880db9a78f7264da26
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-05-19 21:16:28 -0300

    [19/05/2023]Atividade de Álcool ou Gasolina.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿void frase(string txt)
+{
+    for (int i = 0; i < txt.Length; i++)
+    {
+    Console.Write(txt[i]);
+    Thread.Sleep(25); 
+    }
+}
+double calcular(double etan, double gaso)
+{
+    return etan/gaso;
+}
+double checker(double etanol, double gasolina)
+{
+    return calcular(etanol,gasolina);
+}
+Console.ForegroundColor = ConsoleColor.Cyan;
+frase("Qual é o preço do Etanol: ");
+Console.ForegroundColor = ConsoleColor.White;
+double eta = Convert.ToDouble(Console.ReadLine()!.Trim());
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+frase("Qual é o preço do Gasolina: ");
+Console.ForegroundColor = ConsoleColor.White;
+double gas = Convert.ToDouble(Console.ReadLine()!.Trim());
+Console.ForegroundColor = ConsoleColor.Blue;
+frase($"\n\nA porcentagem de Etanol comparando a Gasolina é de {(checker(eta,gas)*100):N1}%\n\nUtilize ");
+if (checker(eta,gas) >= 0.73)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    frase("gasolina.");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    frase("etanol.");
+}
+Console.ResetColor();
